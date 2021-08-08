efce4694a8f14cb02008c467008580ef188de931
Gtihon9 <gtihon9@gmail.com>
2021-08-08 18:20:24 +0300

    FInd_the_odd_int solve

--- Katas/FInd_the_odd_int.py
+++ Katas/FInd_the_odd_int.py
@@ -4,8 +4,8 @@ arr = [[20,1,1,2,2,3,3,5,5,4,20,4,5]
 def find_it(seq):
     _dict = {}
     temp = 0
-    for i in arr:
-        for j in arr:
+    for i in seq:
+        for j in seq:
             if i == j:
                 temp += 1
         if temp % 2 != 0:
